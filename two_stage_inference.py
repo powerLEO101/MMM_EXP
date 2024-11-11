773f404e94130e484ff22ac651148efef1d5ecf4
Leo <powerleo101@gmail.com>
2024-11-11 16:09:02 -0500

    more eval

--- two_stage_inference.py
+++ two_stage_inference.py
@@ -248,49 +248,8 @@ torch.cuda.empty_cache()
 import gc
 gc.collect()
 
-# --- Stage 2 ---
+if master_process:
+    torch.save(top25_ids, 'top25_ids.pt')
+    torch.save(dataloader, 'dataloader.pt')
 
-from vllm import LLM, SamplingParams
-from vllm.lora.request import LoRARequest
-
-def create_query_text(subject_name, construct_name, question_text, mis_name, correct_answer, incorrect_answer) -> str:
-    prompt = (
-        '### Instruction:\n'
-        'You are a math tutor for novice math learners. You are presented with the following math problem in the field of {}, {}. You are given a correct answer and an incorrect answer.\n'
-        'Is the incorrect answer incorrect because of the misconception of {}? Answer only with Yes or No.\n'
-        'Question: {}\n'
-        'Correct answer: {}\n'
-        'Incorrect answer: {}\n'
-        '### Answer:\n'
-    )
-    return prompt.format(subject_name, construct_name, mis_name, question_text, correct_answer, incorrect_answer)
-
-prompts = []
-for idx, row in dataloader.data.iterrows():
-    for i in top25_ids[idx].tolist():
-        text = create_query_text(row['subject_name'], row['construct_name'], row['question_text'], dataloader.misconceptions.iloc[i]['MisconceptionName'], row['correct_answer'], row['incorrect_answer'])
-        prompts.append(text)
-
-llm = LLM(model="Qwen/Qwen2.5-32B-Instruct-AWQ", enable_lora=True, tensor_parallel_size=2)
-
-# --- 
-sampling_params = SamplingParams(max_tokens=1, logprobs=20)
-result = llm.generate(prompts, sampling_params)
-rerank_score = [x.outputs[0].logprobs[0][9454].logprob if 9454 in x.outputs[0].logprobs[0] else -100 for x in result]
-
-# --- 
-
-def rerank_submission(ids, scores):
-    reranks = np.argsort(scores)[::-1]
-    new_ids = [ids[x] for x in reranks]
-    return new_ids
-
-rerank_top25_ids = [rerank_submission(top25_ids[i].tolist(), rerank_score[i * 25 : (i + 1) * 25]) for i in range(len(top25_ids))]
-
-submission = []
-for idx, row in dataloader.data.iterrows():
-    real_id = f"{row['id']}_{row['choice']}"
-    submission.append([real_id, ' '.join([str(x) for x in rerank_top25_ids[idx]])])
-submission = pd.DataFrame(submission, columns=['QuestionId_Answer', 'MisconceptionId'])
-submission.to_csv('submission.csv', index=False)
 # --- End ---
---
+++ two_stage_inference_rerank.py
@@ -0,0 +1,71 @@
+# --- Stage 2 ---
+import os
+import sys
+import math
+import argparse
+import pandas as pd
+import numpy as np
+import torch
+from torch import Tensor
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModel, BitsAndBytesConfig
+from time import time
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    get_peft_model_state_dict,
+    set_peft_model_state_dict,
+)
+import bitsandbytes as bnb
+from torch.distributed import init_process_group, destroy_process_group
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+import argparse
+from vllm import LLM, SamplingParams
+from vllm.lora.request import LoRARequest
+
+top25_ids = torch.load('top25_ids.pt')
+dataloader = torch.load('dataloader.pt')
+
+def create_query_text(subject_name, construct_name, question_text, mis_name, correct_answer, incorrect_answer) -> str:
+    prompt = (
+        '### Instruction:\n'
+        'You are a math tutor for novice math learners. You are presented with the following math problem in the field of {}, {}. You are given a correct answer and an incorrect answer.\n'
+        'Is the incorrect answer incorrect because of the misconception of {}? Answer only with Yes or No.\n'
+        'Question: {}\n'
+        'Correct answer: {}\n'
+        'Incorrect answer: {}\n'
+        '### Answer:\n'
+    )
+    return prompt.format(subject_name, construct_name, mis_name, question_text, correct_answer, incorrect_answer)
+
+prompts = []
+for idx, row in dataloader.data.iterrows():
+    for i in top25_ids[idx].tolist():
+        text = create_query_text(row['subject_name'], row['construct_name'], row['question_text'], dataloader.misconceptions.iloc[i]['MisconceptionName'], row['correct_answer'], row['incorrect_answer'])
+        prompts.append(text)
+
+llm = LLM(model="Qwen/Qwen2.5-32B-Instruct-AWQ", enable_lora=True, tensor_parallel_size=2)
+
+# --- 
+sampling_params = SamplingParams(max_tokens=1, logprobs=20)
+result = llm.generate(prompts, sampling_params)
+rerank_score = [x.outputs[0].logprobs[0][9454].logprob if 9454 in x.outputs[0].logprobs[0] else -100 for x in result]
+
+# --- 
+
+def rerank_submission(ids, scores):
+    reranks = np.argsort(scores)[::-1]
+    new_ids = [ids[x] for x in reranks]
+    return new_ids
+
+rerank_top25_ids = [rerank_submission(top25_ids[i].tolist(), rerank_score[i * 25 : (i + 1) * 25]) for i in range(len(top25_ids))]
+
+submission = []
+for idx, row in dataloader.data.iterrows():
+    real_id = f"{row['id']}_{row['choice']}"
+    submission.append([real_id, ' '.join([str(x) for x in rerank_top25_ids[idx]])])
+submission = pd.DataFrame(submission, columns=['QuestionId_Answer', 'MisconceptionId'])
+submission.to_csv('submission.csv', index=False)
+# --- End ---
